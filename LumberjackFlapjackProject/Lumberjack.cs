6f4d394c66da6ce9b65f5e695eb4b68705b9a844
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-09-02 19:10:01 +0100

    Add project files.

---
+++ LumberjackFlapjackProject/Lumberjack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LumberjackFlapjackProject
+{
+    class Lumberjack
+    {
+        public string Name { get; private set; }
+
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+
+        public Lumberjack(string name)
+        {
+            Name = name;
+        }
+
+        public void TakeFlapjack(Flapjack flapjack)
+        {
+            flapjackStack.Push(flapjack);
+        }
+
+        public void EatFlapjacks()
+        {
+            Console.WriteLine($"{Name} is eating flapjacks");
+            while (flapjackStack.Count > 0)
+            {
+                Console.WriteLine($"{Name} ate a {flapjackStack.Pop().ToString().ToLower()} flapjack");
+            }
+        }
+    }
+}
